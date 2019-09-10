f85e0ccaead0496b8b2dd95aa90d173a1000e0d2
marcrow <marcrowproject@gmail.com>
2019-09-10 13:32:52 +0000

    Added script to launch the web service

---
+++ start.sh
@@ -0,0 +1,38 @@
+#!/bin/bash
+
+certif="/etc/ssl/certs/webserver.crt"
+key="/etc/ssl/private/webserver.key"
+port="4443"
+logfile=/var/log/wssh
+
+#color
+red="\e[31m"
+default="\e[39m"
+
+echo -e $red
+
+systemctl start apache2 2>&1
+
+if [[ $? -ne 0 ]]; then
+	echo "Error : Unable to start apache"
+	exit -1
+fi
+
+
+for pid in $(ps -aux | grep wssh | awk '{print $2}'); do
+	kill $pid 2> /dev/null
+done
+
+if [[ $(du -k $logfile | cut -f1) -ge 5 ]]; then
+	line=$(wc -l $logfile | cut -d ' ' -f 1)
+	middle=$(expr $line / 2)
+	echo "The wssh log file becomes too large."
+	echo "Deleting the first half of the log file...."
+	echo "$middle lines will be removed"
+	sed "1,$middle d" -i $logfile
+fi
+
+echo -e $default
+
+wssh --certfile=$certif --keyfile=$key --sslport=$port &> $logfile &
+
